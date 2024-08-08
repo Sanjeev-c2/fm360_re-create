ddc2d78eab57e658020868f1c26e8e69c0718e8a
Avinash14012002 <avinash.k@codentrix.com>
2024-08-08 12:42:29 +0530

    Modified node re-size

--- FrontEnd/fm_360_fe/src/Components/CustomNodes.jsx
+++ FrontEnd/fm_360_fe/src/Components/CustomNodes.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Handle } from '@xyflow/react';
+import { Handle ,NodeResizer} from '@xyflow/react';
 
-const CustomNode = ({data}) => {
+const CustomNode = ({data,selected}) => {
 
           //   const [imageUrl, setImageUrl] = useState('');
         
@@ -15,17 +15,33 @@ const CustomNode = ({data}) => {
           // }, []);
 
           const imageUrl = data?.image || 'https://via.placeholder.com/50'; // Fallback image URL
-
+// let selected=true;
   return (
-    <div className="customnodes">
-      <div style={{ position: 'relative', width: 30, height: 30 }}>
+    <div className="customnodes" >
+      <div style={{
+        // position:'relative',
+        // right:"30px",
+      width:"60px",height:"20px"}}> 
+      <NodeResizer color="#ff0000"
+        isVisible={selected} 
+        // maxWidth={10} maxHeight={10} 
+        style={{ 
+          width: '100%', height: '100%',
+          // position:"absolute"
+        backgroundSize:"cover"
+         }}/>
+         
         <img
           src={imageUrl} // Fallback image URL
           alt="Node Image"
-          style={{ width: '100%', height: '100%', borderRadius: '4px' }}
+          style={{ width: '100%', height: '100%', borderRadius: '4px',
+            right:"1px",
+            position:"absolute",backgroundSize:"cover"}}
         />
+        {/* </div> */}
         <Handle type="source" position="right"  style={{ background: '#555' }} />
         <Handle type="target" position="left" style={{ background: '#555' }} />
+     
       </div>
     </div>
   );
